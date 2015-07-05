591baaad0192b094a2d957567be892bd6355c058
Wang Junjie <ijunjie.wang@icloud.com>
2015-07-05 12:35:28 +0800

    modified js impl

--- js/mymodule.js
+++ js/mymodule.js
@@ -1,19 +1,9 @@
-/*
- * 这是一个 JavaScript 代码草稿纸。
- *
- * 输入一些 JavaScript，然后可点击右键或从 执行 菜单中选择：
- * 1. 运行 对选中的文本求值(eval) (Ctrl+R)，
- * 2. 查看 对返回值使用对象查看器 (Ctrl+I)，
- * 3. 显示 在选中内容后面以注释的形式插入返回的结果。 (Ctrl+L)
- */
-var mymodule = (function (module) {
+var junjie = (function () {
   var MT = function (rowCountArg) {
-    var rowCount = 0;
-    if (arguments.length == 0) {
-      rowCount = 9;
-    } else if (arguments.length > 1) {
-      throw 'Expected only one argument!';
+    if (arguments.length > 1) {
+      throw 'Only one argument expected!';
     } else {
+      rowCountArg = rowCountArg || 9;
       if (rowCountArg.constructor != Number) {
         throw 'Illegal argument type!';
       } else {
@@ -24,7 +14,6 @@ var mymodule = (function (module) {
           throw 'argument should be between 1 to 9!'
         }
       }
-      rowCount = rowCountArg;
     }
     var makeRowStr = function (rowIndex) {
       var arr = [
@@ -37,7 +26,7 @@ var mymodule = (function (module) {
     this.toString = function () {
       var arr = [
       ];
-      for (var count = 1; count < rowCount + 1; count++) {
+      for (var count = 1; count < rowCountArg + 1; count++) {
         arr.push(makeRowStr(count));
       }
       return arr.join('\r\n');
@@ -46,6 +35,6 @@ var mymodule = (function (module) {
   return {
     MultiplicationTable: MT
   };
-}) ({});
-var table = new mymodule.MultiplicationTable(8);
+}) ();
+var table = new junjie.MultiplicationTable(4);
 console.log(table.toString());
