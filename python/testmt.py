5cf34a23d6ef8bf52cd97a0cacd2e9eb9229fe18
Wang Junjie <ijunjie.wang@icloud.com>
2015-07-05 12:35:05 +0800

    added python impl

---
+++ python/testmt.py
@@ -0,0 +1,9 @@
+__author__ = 'Ivo'
+from mt import MutltiplicationTable
+if __name__ == '__main__':
+    n = 6
+    table_n = MutltiplicationTable(n)
+    print(table_n)
+
+    table = MutltiplicationTable()
+    print(table)
