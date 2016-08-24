fe67e5050c9e3551d7202cfe5e6c281ea2d85af6
Wang Junjie <ijunjie.wang@icloud.com>
2016-08-24 20:03:04 +0800

    Refactored
    use only one line of code to print a multiplication table

--- scala/src/mt/MultiplicationTable.scala
+++ scala/src/mt/MultiplicationTable.scala
@@ -1,15 +1,5 @@
 package mt
 
-class MultiplicationTable(rowCount: Int = 9) {
-  require(rowCount > 0 && rowCount < 10)
-  override def toString = {
-    def rowStr(rowIndex: Int) = {
-      val rowGenerator = for (col <- 1 to rowIndex) 
-        yield col + "*" + rowIndex + "=" + col * rowIndex
-      rowGenerator.mkString("\t")
-    }
-    val tableGenerator = for (count <- 1 to rowCount) 
-      yield rowStr(count)
-    tableGenerator.mkString("\r\n")
-  }
+class MultiplicationTable extends App {
+  print(1 to 9 map (i=>1 to i map(c=>s"${c}x${i}=${c*i}") mkString "\t") mkString "\r\n")
 }
