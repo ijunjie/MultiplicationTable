29ee3977ba5b1c2ab7854d8e49438236f51aa217
Wang Junjie <ijunjie.wang@icloud.com>
2016-08-24 22:04:54 +0800

    rename file
    rename file

--- scala/src/mt/MultiplicationTable.scala
+++
@@ -1,5 +0,0 @@
-package mt
-
-class MultiplicationTable extends App {
-  print(1 to 9 map (i=>1 to i map(c=>s"${c}x${i}=${c*i}") mkString "\t") mkString "\r\n")
-}
---
+++ scala/src/mt/multiplication_table.sc
@@ -0,0 +1,1 @@
+print(1 to 9 map (i=>1 to i map(c=>s"${c}x${i}=${c*i}") mkString "\t") mkString "\r\n")
